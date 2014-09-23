163c271d22e7471000e104154405259f4821d82d
jfryan7 <jfryan@ualberta.ca>
2014-09-22 22:51:56 -0600

    Changed the views

--- gen/ca/ualberta/cs/jfryanToDo/R.java
+++ gen/ca/ualberta/cs/jfryanToDo/R.java
@@ -25,21 +25,26 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int archived_todo_listview=0x7f080000;
+        public static final int AddButton=0x7f080003;
+        public static final int AddEditText=0x7f080002;
+        public static final int ArchivedTextView=0x7f080000;
+        public static final int ToDoListView=0x7f080004;
+        public static final int ToDoTextView=0x7f080001;
+        public static final int action_settings=0x7f080005;
     }
     public static final class layout {
-        public static final int archived_todos=0x7f030000;
+        public static final int archived=0x7f030000;
         public static final int main_todos=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int Add=0x7f050004;
+        public static final int Archived=0x7f050002;
         public static final int Todo=0x7f050003;
-        public static final int action_settings=0x7f050002;
+        public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
     }
     public static final class style {
         /** 
